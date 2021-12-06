e66086ad973dd19631216d25c539fd84c6050047
nlttuyen <95175915+nlttuyen@users.noreply.github.com>
2021-12-06 13:08:38 +0700

    Add external icons

--- src/app.js
+++ src/app.js
@@ -30,48 +30,46 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-// function displayForecast(response) {
-//   let forecast = response.data.daily;
+function displayForecast(response) {
+  let forecast = response.data.daily;
 
-//   let forecastElement = document.querySelector("#forecast");
+  let forecastElement = document.querySelector("#forecast");
 
-//   let forecastHTML = `<div class="row">`;
-//   forecast.forEach(function (forecastDay, index) {
-//     if (index < 6) {
-//       forecastHTML =
-//         forecastHTML +
-//         `
-//       <div class="col-2">
-//         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-//         <img
-//           src="http://openweathermap.org/img/wn/${
-//             forecastDay.weather[0].icon
-//           }@2x.png"
-//           alt=""
-//           width="42"
-//         />
-//         <div class="weather-forecast-temperatures">
-//           <span class="weather-forecast-temperature-max"> ${Math.round(
-//             forecastDay.temp.max
-//           )}° </span>
-//           <span class="weather-forecast-temperature-min"> ${Math.round(
-//             forecastDay.temp.min
-//           )}° </span>
-//         </div>
-//       </div>
-//   `;
-//     }
-//   });
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="images/${forecastDay.weather[0].icon}.png"
+          alt=""
+          width="30"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
 
-//   forecastHTML = forecastHTML + `</div>`;
-//   forecastElement.innerHTML = forecastHTML;
-// }
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
-// function getForecast(coordinates) {
-//   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-//   axios.get(apiUrl).then(displayForecast);
-// }
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -92,7 +90,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `images/${response.data.weather[0].icon}.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
@@ -114,4 +112,4 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-search("New York");
+search("Barcelona");
